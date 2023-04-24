b092c439e059c7a87595e2da464048dd06d3f491
paaaarv <parvathikumar92@gmail.com>
2023-04-24 15:59:58 -0500

    ability to pick date on calendar

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react"; 
+import React, {useState} from 'react';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import dayjs, { Dayjs } from 'dayjs';
 
 export default function Appointment() {
+    const [value, setValue] = useState(dayjs());
 
     const showAppointments = () =>  { 
         let timeArrays= ['10:30', '11:00', '11:30', '12:00', '12:30']; 
@@ -13,12 +15,16 @@ export default function Appointment() {
             })
     };
 
-   
-
     return(  
     <div className="flex popup"> 
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateCalendar /> 
+        <DateCalendar 
+            disablePast={true}
+            views={['day']}
+            onChange={(newValue) => {setValue(newValue)}}
+            value={value}
+
+        /> 
         </LocalizationProvider>
     </div>
     )
